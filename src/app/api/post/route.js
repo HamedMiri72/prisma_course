dc1d8589bd0f58635eef31296daa5661bf8c59b5
HamedMiri72 <hamedmiri72@gmail.com>
2025-04-04 11:06:03 +0100

    advanced queries

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -3,7 +3,27 @@ import { prisma } from "@/lib/prisma"
 
 export const GET = async() => {
 
-    const posts = await prisma.post.findMany();
-    return new Response(JSON.stringify(posts));
+    const posts = await prisma.post.findMany({
+        where:{
+            OR:[
 
+                {
+                    title:{
+                        contains:"Github",
+                        mode :"insensitive" // makes the feild insensetive to lower case and uppercase
+                    },
+                },
+                {
+                    title: {
+                        contains: "Twitter"
+                    },
+
+                },
+            ],
+            AND:{
+                published: true
+            },
+        },
+    })
+    return new Response(JSON.stringify(posts));
 }
---
+++ src/app/api/user/route.js
@@ -0,0 +1,52 @@
+import { prisma } from "@/lib/prisma";
+
+
+
+
+export const GET = async () => {
+    const users = await prisma.user.findMany({
+        where: {
+            // name : {
+            //     // startsWith: "J"
+            //     // endsWith: "n",
+            //     // contains: "a",
+            //     // startsWith: "s"
+            // }
+
+            // id :{
+            //     // in: [1,2]
+            //     notIn: [1,2]
+            // }
+            // id:{
+            //     not:{
+            //         in:[1,2]
+            //     }
+            // }
+
+            // id: {
+            //     not:{
+            //         gt: 2,
+            //     }
+            // }
+
+            // OR:[
+            //     {
+            //         id:{
+            //             not:{
+            //                 gt:2,
+            //             }
+            //         },
+                
+            //     },
+            //     {
+            //         name: {
+            //             startsWith: "s"
+            //         }
+            //     }
+            // ]
+            
+        }
+    })
+
+    return new Response(JSON.stringify(users));
+}
